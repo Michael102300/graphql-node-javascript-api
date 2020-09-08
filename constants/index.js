62d1f6bf80d7ac136c212b969ddad0f104c61fd6
Michael <michaelolivares123@gmail.com>
2020-09-08 11:26:35 -0500

    finally commit

---
+++ constants/index.js
@@ -0,0 +1,84 @@
+module.exports.recipes = [
+    {
+        id: "1",
+        name: "Pollo asado",
+        description: "Pollo asado description",
+        ingredients: "pollo asado ingredients",
+        categoryName: "Comida principal",
+        userId: "3",
+    },
+    {
+        id: "2",
+        name: "Pizza",
+        description: "Pizza description",
+        ingredients: "Pizza ingredients",
+        categoryName: "Otros",
+        userId: "1"
+    },
+    {
+        id: "3",
+        name: "Hamburguer",
+        description: "Hamburguer description",
+        ingredients: "Hamburguer ingredients",
+        categoryName: "Postre",
+        userId: "4"
+    },
+    {
+        id: "4",
+        name: "Water",
+        description: "water description",
+        ingredients: "water ingredients",
+        categoryName: "Jugos",
+        userId: "2"
+    },
+    {
+        id: "5",
+        name: "Pan",
+        description: "Pan description",
+        ingredients: "Pan ingredients",
+        categoryName: "Otros",
+        userId: "3"
+    },
+];
+
+module.exports.users = [
+    {
+        id: "1",
+        name: "kevin",
+        email: "kevin@gmail.com"
+    },
+    {
+        id: "2",
+        name: "john",
+        email: "jhonn@gmail.com"
+    },
+    {
+        id: "3",
+        name: "peter",
+        email: "peter@gmail.com"
+    },
+    {
+        id: "4",
+        name: "bob",
+        email: "bob@gmail.com"
+    },
+];
+
+module.exports.categorys = [
+    {
+        id: "1",
+        name: "Comida principal",
+    },
+    {
+        id: "2",
+        name: "Postre",
+    },
+    {
+        id: "3",
+        name: "Jugos",
+    },
+    {
+        id: "4",
+        name: "Otros",
+    },
+];
